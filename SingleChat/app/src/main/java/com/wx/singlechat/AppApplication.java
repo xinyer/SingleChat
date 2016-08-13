5932a15cc08fa0c95977d52c7589c273f3d72da0
wangxin <wangxin@wisdomgarden.com>
2016-08-14 00:21:57 +0800

    add tencent im sdk

---
+++ SingleChat/app/src/main/java/com/wx/singlechat/AppApplication.java
@@ -0,0 +1,13 @@
+package com.wx.singlechat;
+
+import android.app.Application;
+
+public class AppApplication extends Application {
+
+    public static String TENCENT_IM_SDK_APPID = "1400013433";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
